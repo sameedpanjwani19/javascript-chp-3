cd487d11e856e1d9051f5ee7f4fe49c47d4c6a28
abdulsameed19 <154329359+abdulsameed19@users.noreply.github.com>
2024-03-04 10:39:26 +0500

    chp-3

---
+++ main.js
@@ -0,0 +1,14 @@
+
+var age = 25;
+
+alert("My age is: " + age);
+
+var visits = 0;
+
+visits++;
+
+document.write("You have visited this site " + visits + " times.");
+
+var birthYear = 1990;
+
+document.write(birthYear);
